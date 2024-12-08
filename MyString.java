ae628bc403af9b30af557cabfacd893d290bac18
tal.aizenkraft <tal.aizenkraft@tufin.com>
2024-12-08 18:41:35 +0200

    mystring.java

--- MyString.java
+++ MyString.java
@@ -7,8 +7,13 @@ public class MyString {
         System.out.println(countChar(hello, 'h'));
         System.out.println(countChar(hello, 'l'));
         System.out.println(countChar(hello, 'z'));
+        System.out.println(countChar(hello, 'H'));
+        System.out.println(subsetOf("rruni","running"));
+        System.out.println(spacedString("silent"));
+        System.out.println(subsetOf("pass","space"));
         System.out.println(spacedString(hello));
-        //// Put your other tests here.
+        System.out.println(randomStringOfLetters(5));
+        System.out.println(remove("committee","meet"));
     }
 
     /**
@@ -20,8 +25,16 @@ public class MyString {
      * @return the number of times c appears in str
      */
     public static int countChar(String str, char ch) {
-        //// Replace the following statement with your code
-        return 0;
+        int charAsciiValue = (int) ch;
+        int currentAsciiValue = 0;
+        int counter = 0;
+
+        for (int i = 0; i < str.length(); i++){
+            currentAsciiValue = (int) str.charAt(i);
+            if (currentAsciiValue == charAsciiValue) counter++;
+        }
+
+        return counter;
     }
 
     /** Returns true if str1 is a subset string str2, false otherwise
@@ -36,8 +49,25 @@ public class MyString {
      * @return true is str1 is a subset of str2, false otherwise
      */
     public static boolean subsetOf(String str1, String str2) {
-         //// Replace the following statement with your code
-        return false;
+        boolean removed = false;
+        String subString = "";
+
+        for (int i = 0; i < str1.length(); i ++){
+            subString = "";
+            removed = false;
+
+            if (countChar(str2, str1.charAt(i)) < 1) return false;
+
+            for (int j = 0; j < str2.length(); j++){
+                if (!removed && str2.charAt(j) == str1.charAt(i)){
+                    removed = true;
+                } else{
+                    subString += str2.charAt(j); 
+                }
+            }
+            str2 = subString;
+        }
+        return true;
     }
 
     /** Returns a string which is the same as the given string, with a space
@@ -49,8 +79,11 @@ public class MyString {
      * @return a string consisting of the characters of str, separated by spaces.
      */
     public static String spacedString(String str) {
-        //// Replace the following statement with your code
-        return null;
+        String finalString = "";
+        for (int i = 0; i < str.length(); i ++){
+            finalString += (i == str.length() - 1) ? str.charAt(i) : str.charAt(i) + " ";
+        }
+        return finalString;
     }
   
     /**
@@ -64,8 +97,15 @@ public class MyString {
      * @return a randomly generated string, consisting of 'n' lowercase letters
      */
     public static String randomStringOfLetters(int n) {
-        //// Replace the following statement with your code
-        return null;
+        String randomString = "";
+        int randomAsciiValue = 0;
+
+        for(int i = 0; i < n; i ++){
+            randomAsciiValue = 97 + (int) (Math.random() * 26);
+            randomString += (char) randomAsciiValue; 
+        }
+
+        return randomString;
     }
 
     /**
@@ -78,8 +118,25 @@ public class MyString {
      * @return a string consisting of str1 minus all the characters of str2
      */
     public static String remove(String str1, String str2) {
-       //// Replace the following statement with your code
-        return null;
+        boolean removed = false;
+        String subString = "";
+
+        for (int i = 0; i < str2.length(); i ++){
+            subString = "";
+            removed = false;
+
+            if (countChar(str1, str2.charAt(i)) < 1) continue;
+
+            for (int j = 0; j < str1.length(); j++){
+                if (!removed && str1.charAt(j) == str2.charAt(i)){
+                    removed = true;
+                } else{
+                    subString += str1.charAt(j); 
+                }
+            }
+            str1 = subString;
+        }
+        return str1;
     }
 
     /**
